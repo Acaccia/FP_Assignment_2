088396e1968c045eb2eddd2be6a156b16e1377e8
acaccia <acaccia@ulb.ac.be>
2018-01-10 18:12:59 +0100

    worms + several functions to deal with them

--- src/Data/Internal/Direction.hs
+++ src/Data/Internal/Direction.hs
@@ -2,8 +2,13 @@ module Data.Internal.Direction where
 
 import Data.Bifunctor
 import Data.Internal.Nat
+import System.Random
 
 data Direction = U | D | L | R deriving (Bounded, Enum, Eq, Show)
+
+instance Random Direction where
+  randomR (a, b) = first toEnum . randomR (fromEnum a, fromEnum b)
+  random = randomR (minBound, maxBound)
 
 move :: Enum a => Direction -> (a, a) -> (a, a)
 move U = first pred
--- src/Data/Worm.hs
+++ src/Data/Worm.hs
@@ -1,14 +1,36 @@
+{-# LANGUAGE Strict #-}
 module Data.Worm where
 
-import qualified Data.Internal.Direction as D
-import           Data.Internal.Nat
+import Control.Concurrent.STM
+import Data.Internal.Direction
+import Data.Internal.Nat
+import System.Random
 
 data Worm = Worm {
-    headPos   :: (Nat, Nat)
-  , direction :: D.Direction
-  , size      :: Nat
+    corpse    :: [(Nat, Nat)]
+  , direction :: Direction
   , growing   :: Bool
   }
 
-continueGrow :: Nat -> Worm -> Worm
-continueGrow maxS (Worm pos dir s g) = Worm pos dir s (g && s < maxS)
+type TWorm = TVar Worm
+type TWorms = [TWorm]
+
+initWorm :: (Nat, Nat) -> StdGen -> (Worm, StdGen)
+initWorm p g = let (d, g') = random g in (Worm [p] d True, g')
+
+grow, dug :: Worm -> Worm
+grow (Worm cor@(c:_) d g) = Worm (move U c : cor) d g
+dug (Worm cs d g) = Worm (init cs) d g
+
+living :: Worm -> Bool
+living = not . null . corpse
+
+keepGrowing :: Int -> Worm -> Worm
+keepGrowing maxSize (Worm cs d g) = Worm cs d (g && length cs < maxSize)
+
+growSTM, dugSTM :: TWorm -> STM ()
+growSTM = flip modifyTVar' grow
+dugSTM = flip modifyTVar' dug
+
+corpsePositionsSTM :: TWorms -> STM [(Nat, Nat)]
+corpsePositionsSTM = fmap concat . traverse (fmap corpse . readTVar)

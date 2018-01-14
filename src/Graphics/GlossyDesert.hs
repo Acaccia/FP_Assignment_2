f8ff23e46aabe873f4a3b4aa12603f64c78f7fdf
acaccia <acaccia@ulb.ac.be>
2018-01-14 21:18:03 +0100

    end of drawings

--- src/Graphics/GlossyDesert.hs
+++ src/Graphics/GlossyDesert.hs
@@ -3,6 +3,7 @@ module Graphics.GlossyDesert where
 
 import Control.Lens
 import Data.Desert
+import Data.Internal.Nat
 import Data.Player
 import Data.Worm
 import Graphics.Gloss
@@ -31,15 +32,26 @@ tileToPicture t = case t of
     Just Lava     -> rect (dark red)
   where rect col = color col (rectangleSolid 40 40)
 
-drawingDesert :: [Maybe Tile] -> Picture
-drawingDesert ts = pictures $ zipWith (uncurry translate) [(x, y) | y <- [480,440..(-480)], x <- [(-480), (-440)..480]] (tileToPicture <$> ts)
+drawingDesert :: (Nat, Nat) -> Desert -> Picture
+drawingDesert p = pictures . zipWith (uncurry translate) [(x, y) | y <- [480,440..(-480)], x <- [(-480), (-440)..480]] . fmap tileToPicture . surroundingsArray p 12 12
+
+drawingPlayer :: (Nat, Nat) -> Picture
+drawingPlayer (i, j) = translate x y $ color black $ circleSolid 15
+  where x = if i >= 12 then 0 else [(-480), (-440)..] !! fromEnum i
+        y = if j >= 12 then 0 else [480, 440..] !! fromEnum j
+
+drawingWorms :: (Nat, Nat) -> [(Nat, Nat)] -> Picture
+drawingWorms (i, j) = pictures . fmap (draw . relativeCoord) . filter inRange
+  where inRange (x, y) = i - 12 < x && i + 12 > x && j - 12 < y && j + 12 > y
+        relativeCoord (x, y) = ([(-480), (-440)..480] !! fromEnum (x - i), [480,440..(-480)] !! fromEnum (y - j))
+        draw (x, y) = translate x y $ color (greyN 0.4) $ circleSolid 20
 
 window = InWindow "Desert" (1000, 1000) (0, 0)
 
 background = black
 
-drawing = drawingDesert (surroundingsArray (0, 0) 12 12 testDesert)
+drawing (i, j) = pictures [drawingDesert (i, j) testDesert, drawingPlayer (i, j), drawingWorms (i, j) [(1, 1), (4, 5), (5000, 12)]]
 drawing' = color white $ scale 0.35 0.35 $ text "1"
 
 main :: IO ()
-main = display window background drawing
+main = display window background $ drawing (0, 0)
